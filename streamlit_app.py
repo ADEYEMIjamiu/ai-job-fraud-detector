3a31d73e8bfc45d36b6c54066e4f131ea16e3564
ADEYEMI J. ADEGBENRO <Adeyemi.Adegbenro@gisma-student.com>
2026-08-20 18:05:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import joblib
 import re
-import sklearn
-import numpy
-
-st.write("scikit-learn:", sklearn.__version__)
-st.write("joblib:", joblib.__version__)
-st.write("numpy:", numpy.__version__)
 
 svm = joblib.load('svm_model.joblib')
 vectorizer = joblib.load('tfidf_vectorizer.joblib')
